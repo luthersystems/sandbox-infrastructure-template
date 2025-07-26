aa4a7391c42d31bbbe685014f0bbf4986a22dc9c
sam-at-luther <sam.wood@luthersystems.com>
2025-07-26 11:30:59 -0700

    Fix path

--- pull.sh
+++ pull.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 SCRIPT_SOURCE="${BASH_SOURCE[0]:-$0}"
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_SOURCE")" && pwd)"
-MARS_PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+MARS_PROJECT_ROOT="$(cd "$SCRIPT_DIR" && pwd)"
 
 . "$MARS_PROJECT_ROOT/shell_utils.sh"
 
