f67fe68179642c1aa364e5ffdd62d9d792b071db
sam-at-luther <sam.wood@luthersystems.com>
2025-10-02 17:53:12 -0700

    Remove duplicate var

--- tf/custom-stack-provision/__customer_state_inputs.tf
+++ tf/custom-stack-provision/__customer_state_inputs.tf
@@ -1,6 +1,6 @@
 # Inputs needed to read the cloud-provision remote state and choose the role/region
 variable "short_project_id" { type = string }
-variable "aws_region" { type = string }
+# variable "aws_region" { type = string } # this is already in the customer TF
 variable "bootstrap_state_bucket" { type = string }
 variable "bootstrap_state_region" { type = string }
 variable "bootstrap_state_env" { type = string }
